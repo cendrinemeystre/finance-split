8ddd52a1d3870737468ebfefac5fb5bed666798a
Cendrine Meystre <mcendrine27@gmail.com>
2026-03-04 22:04:47 +0100

    fix: fix api url

--- finance-split-backend/src/main/java/com/nexushub/finance/split/backend/config/SpringMvcConfig.java
+++ finance-split-backend/src/main/java/com/nexushub/finance/split/backend/config/SpringMvcConfig.java
@@ -13,7 +13,7 @@ public class SpringMvcConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+          .allowedOrigins("*")
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
