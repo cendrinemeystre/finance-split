95462a1be23108ededf70a8d018bf294a46c78e9
Cendrine Meystre <mcendrine27@gmail.com>
2026-03-04 01:28:49 +0100

    fix: cors

--- finance-split-backend/src/main/java/com/nexushub/finance/split/backend/config/SpringMvcConfig.java
+++ finance-split-backend/src/main/java/com/nexushub/finance/split/backend/config/SpringMvcConfig.java
@@ -13,7 +13,7 @@ public class SpringMvcConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins("http://localhost:4200")
+          .allowedOrigins("http://localhost:4200", "http://localhost:8080")
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
